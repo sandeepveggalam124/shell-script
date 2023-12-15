033c3d17cfbfc58378780e5942a80c512dfd800c
sandeepveggalam124 <sandeepveggalam124@gmail.com>
2023-12-15 18:16:57 +0530

    second

--- 5-var.sh
+++ 5-var.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 echo "Please enter your username:: "
-
 read -s USERNAME
+
+echo "Please enter your Password:: "
+read -s PASSWORD
 
 echo "username is : $USERNAME, password is : $PASSWORD"
 
