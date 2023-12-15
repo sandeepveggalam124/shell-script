c68db074a5e4ec6de45cf01fa94e6e7134b210ae
sandeepveggalam124 <sandeepveggalam124@gmail.com>
2023-12-15 17:55:44 +0530

    second

--- 4-var.sh
+++ 4-var.sh
@@ -2,8 +2,10 @@
 
 PERSON1=$1
 PERSON2=$2
+PERSON3=$3
 
 echo "$PERSON1: Hello $PERSON2, Good Morning"
 echo "$PERSON2: Hi $PERSON1, very Good Morning"
 echo "$PERSON1: How are you $PERSON2?"
 echo "$PERSON2: I am good $PERSON1, How are you"
+echo "$PERSON3: I am good $PERSON3, How are you"
